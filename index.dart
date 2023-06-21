387c242852f804b899aa886b70fbde146ee314af
[lince_academye] <[julianokeil@gmail.com]>
2023-06-20 22:01:00 -0300

    aula de variaveis

---
+++ index.dart
@@ -0,0 +1,80 @@
+void main() {
+  //variaveis!
+
+  // var nomeCompleto ='joao da silva'; // a variavel var sera declarada comforme a sua atribuição
+  // Object nomecompleto = 'joao da silva '; //
+  //String nomeCompleto = 'joao da silva';explicito
+  //dynamic nomeCompleto = 'joao da silva';dinamico
+  //nomeCompleto = 'joao da silva';
+
+  // print(nomeCompleto);
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+  // valores nulos
+
+  // String? nome= null;     // atribuir ? a variavel !!
+
+  // print(nome);
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+  //  late variais
+
+  // late double resultado = _getValorTotal();
+
+  // resultado.toInt();
+
+  // print('chamando a func main');
+
+//double _getValorTotal() {
+  // print('chamando o func main');
+
+//  return 8.0;
+//}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+  // modificadores const e final
+
+  //final listaDelinguages = ['c#', 'java'];
+  //const listaDelinguages = ['c#', 'java'];
+
+  // listaDelinguages.add('ruby');
+
+  //print(listaDelinguages);
+
+  //-----------------------------------------------------------------------------------------------------------------------------
+
+// tipos entregados, numers
+
+  //int idade = 12;
+
+  //ler os documentos
+
+  //print(idade);
+
+  //-----------------------------------------------------------------------------------------------------------------------------
+
+  //trabalhando com texto
+
+  // String string1 = 'it\'s great'; // ou " "
+
+  //String linguagem = 'c#';
+
+  //String string2 = 'estou estudando a linguagem ${linguagem.toUpperCase()} ';
+
+  // print(string2);
+
+  //-----------------------------------------------------------------------------------------------------------------------------
+
+  //tipos integrados
+
+  // bool ativo = false;
+  // String nome = 'D';
+  // print(ativo.toString());
+
+  // if (nome.contains('D')) {
+  //   print('Activo e diferente');
+  // }
+}
