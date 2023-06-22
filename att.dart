5a2b7bfa3b3974a1abb2c9eaafb999af1ffeded0
juliano keil <117609920+Julliano-keil@users.noreply.github.com>
2023-06-21 22:50:16 -0300

    Update att.dart

---
+++ att.dart
@@ -0,0 +1,42 @@
+//void main() {
+//   // var mato = <String>{};
+//   Set<String> mato = {};
+//   print(mato);
+
+//----------------------------------------------------------------------------------------------------
+
+  //Maps
+
+  // Map<String, String> comentarios = Map<String, String>();
+  // comentarios['java'] = 'comentario java';
+  // comentarios['c#'] = 'comentario C#';                  //lista
+  // comentarios['Dart'] = 'comentario dart';
+  //key                     //value
+  // comentarios.values.forEach((element) {          //forma 1
+  //   print(element);
+  // });
+  // comentarios.forEach((key, value) {              //forma 2
+  //   print('$key => $value');
+  // });
+
+  // for (var entry in comentarios.entries) {        //forma 3
+  //   print(entry.key);
+  //   print(entry.value);
+  // }
+
+//   Map<String, List<String>> livros = Map<String, List<String>>();
+
+//   livros['java'] = ['java 1 ', 'java 2', 'java 3'];
+//   livros['java'] = ['java 4 ', 'java 5', 'java 6'];
+//   livros['java'] = ['java 7 ', 'java 8', 'java 9'];
+
+//   livros.forEach((key, value) {
+//     print("""
+// $key,
+// $value
+// """);
+//   });
+//    Map<String, List<String>>? teste = null;
+
+//   var novosLivros = {...livros,...?teste};
+// }
